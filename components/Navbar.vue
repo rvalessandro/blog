577560d63eab0854ab6d3581dc468051a2852d20
rvalessandro <ralessandro939@gmail.com>
2020-07-21 12:27:18 +0800

    Added navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="h-16 container mx-auto">
+    <div class="flex justify-between items-center h-full w-full">
+      <div></div>
+
+      <ul class="font-sans tracking-wider">
+        <nuxt-link
+          to="/blog"
+          class="mx-4 uppercase text-sm font-semibold text-gray-600"
+          >blog</nuxt-link
+        >
+        <nuxt-link
+          to="/blog"
+          class="mx-4 uppercase text-sm font-semibold text-gray-600"
+          >projects</nuxt-link
+        >
+        <nuxt-link
+          to="/blog"
+          class="mx-4 uppercase text-sm font-semibold text-gray-600"
+          >about</nuxt-link
+        >
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
